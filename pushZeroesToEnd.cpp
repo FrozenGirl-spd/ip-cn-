2a1dbc0b35769947ff4121faba5dde7a8f12f29a
Silla Priyadarshni <51441069+FrozenGirl-spd@users.noreply.github.com>
2021-10-07 02:08:31 +0530

    Add files via upload

---
+++ pushZeroesToEnd.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int a[]={1, 0, 1, 1,5,0,6, 1,8, 9,0, 1};
+    int a_size=sizeof(a)/sizeof(a[0]);
+     for(int i=0;i<a_size;i++){
+        if(a[i]!=0 ){
+            cout<<a[i]<<" ";
+        }
+    }
+    for(int i=0;i<a_size;i++){
+        if(a[i]==0){
+            cout<<a[i]<<" ";
+        }
+    }
+   
+
+
+return 0;
+}
