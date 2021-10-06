12e1b671302b8d305f7d7b4a5dd7053bbadb6c69
Silla Priyadarshni <51441069+FrozenGirl-spd@users.noreply.github.com>
2021-10-06 21:16:22 +0530

    Add files via upload

---
+++ repeatingArrayElement.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+    int a[]={1,4,7,7,4,9,6,1,2,2};
+    int a_size=sizeof(a)/sizeof(a[0]);
+    for(int i=0;i<a_size;i++){
+        int j;
+        for( j=i+1;j<a_size;j++){
+            if(i!=j && a[i]==a[j]){
+                cout<<a[i];
+                break;
+            }
+           
+        }
+         
+    }
+
+
+return 0;
+}
